89e57ac6ef0345a95d94f0e409e035835471016d
Tekalign Amera <tekalign1991@gmail.com>
2023-03-07 13:38:35 -0800

    two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n < 10:
-        print("0{}, " .format(a), end="")
+        print("0{}," .format(n), end=" ")
     else:
-        print("{}, " .format(a), end="")
+        print("{}," .format(n), end=" ")
