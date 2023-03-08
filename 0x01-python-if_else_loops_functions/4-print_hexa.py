faa0d144dea3f9fc128974be1b8962ca7e42c4c0
Tekalign Amera <tekalign1991@gmail.com>
2023-03-08 05:31:30 -0800

    decimal to hexa

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+for n in range(0, 99):
+    for i in range(97, 103)
+    if n < 10:
+        print("{n} = 0*{n}")
+    elif n > 9 and n < 17:
+        for i in range(97, 103):
+            print("{n} = 0*{chr(i)}")
+    else:
+        print("{n} = 0*{n / 16}{n %16}")
