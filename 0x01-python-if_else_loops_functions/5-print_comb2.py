b6b8da8cdf51b4ddcbe81b50bff5a07a0cd99c5a
Tekalign Amera <tekalign1991@gmail.com>
2023-03-08 13:09:53 -0800

    combo of nums

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    if n < 10:
-        print("0{}, " .format(n), end="")
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{}, " .format(n), end="")
+        print("{:02}".format(number), end=", ")
