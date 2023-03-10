30dd608274bbb18196090505e84c99754b945187
Tekalign Amera <tekalign1991@gmail.com>
2023-03-10 12:10:24 -0800

    executing module as script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
+from add_o import add:
+    a  = 1
     b = 2
-    print(add(a, b)
+    print("{}, + {}. .format(a, b, add(a, b)))
