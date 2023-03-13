0ed3741daf5161a3ca5594dfbef416683ebd65ce
Tekalign Amera <tekalign1991@gmail.com>
2023-03-13 11:47:30 -0700

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for n in range(0, 101):
         if n == 100:
-            print(f"{n}")
-        if n % 3 == 0:
+            print("Buzz")
+        elif n % 3 == 0:
             print("Fizz", end=", ")
         elif n % 5 == 0:
             print("Buzz", end=", ")
