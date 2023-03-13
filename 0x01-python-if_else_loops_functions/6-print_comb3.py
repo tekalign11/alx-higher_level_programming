f70397f3608822ce2b18015348caf23b42fd007f
Tekalign Amera <tekalign1991@gmail.com>
2023-03-13 11:20:32 -0700

    printing combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n in range(0, 9):
     for r in range(0, 10):
         if n == 8 and r == 9:
-            print("{}{}".format(n,r))
+            print("{}{}".format(n, r))
         elif n < r:
-            print("{}{}".format(n,r), end=", ")
+            print("{}{}".format(n, r), end=", ")
