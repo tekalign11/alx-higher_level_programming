f04cc671928090d4dabcadf79b0454d987e634ec
Tekalign Amera <tekalign1991@gmail.com>
2023-03-13 10:33:03 -0700

    print to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+    if n == 99:
+        print("{}".format(n))
+    elif:
+        print("{:02}".format(n), end=", ")
