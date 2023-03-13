af69c84c3a9ec047df89b39734c6fdcf24009ad6
Tekalign Amera <tekalign1991@gmail.com>
2023-03-13 14:08:06 -0700

    retrive element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in range(len(my_list)):
+        if index(i) < 0 or index(i) > (len(my_list -1):
+            return (None)
+        else:
+            return(mylist(len(i)))
